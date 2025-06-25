b7fd36487b9ba7cbcb1cb42d455a8cfdb21cab13
Mac <mac@M1-MBA.local>
2025-06-25 14:36:56 +0900

    add 01_Count

---
+++ Count/Count/CountApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountApp.swift
+//  Count
+//
+//  Created by Mac on 2025/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
