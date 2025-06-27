5e94fc33272705002080699b173ef6911683eba6
Mac <mac@M1-MBA.local>
2025-06-27 15:02:52 +0900

    add Gacha

---
+++ Gacha/Gacha/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Gacha
+//
+//  Created by Mac on 2025/06/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var showSheet = false
+
+    var body: some View {
+        ZStack {
+            Image("gacha")
+                .resizable()
+                .ignoresSafeArea()
+            Button {
+                showSheet = true
+            } label: {
+                Image("Presentbox")
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
+        .fullScreenCover(isPresented: $showSheet) {
+            ResultView()
+        }
+
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gacha/Gacha/GachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GachaApp.swift
+//  Gacha
+//
+//  Created by Mac on 2025/06/27.
+//
+
+import SwiftUI
+
+@main
+struct GachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
