fd4a20b112773b8d4aecb8aa6d1ebadd68b82367
Mac <mac@M1-MBA.local>
2025-06-28 01:56:44 +0900

    add Shake

---
+++ Shake/Shake/ShakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeApp.swift
+//  Shake
+//
+//  Created by Mac on 2025/06/28.
+//
+
+import SwiftUI
+
+@main
+struct ShakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}
