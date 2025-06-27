abf27ebd5574d425a51b826007b6bc789b1d3d7d
Mac <mac@M1-MBA.local>
2025-06-28 01:24:34 +0900

    add PhotoMaster

---
+++ PhotoMaster/PhotoMaster/PhotoMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterApp.swift
+//  PhotoMaster
+//
+//  Created by Mac on 2025/06/27.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
